590c5b059d7db682f947c9cc2732745af8232794
letsaemokhutli <mokhutli.letsae@vodacom.co.ls>
2024-04-18 02:16:17 +0200

    ETL_PIPELINE_DONE

--- interface.py
+++ interface.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello world,344344 from Flask!"
-if __name__ == "__main__":
+    return render_template("index.html")
+if __name__ ==  "__main__" :
     app.run(debug=True)
+
