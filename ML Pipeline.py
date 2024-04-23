794213be59c6cebcd12ba8c3133e1ec49593ab93
letsaemokhutli <mokhutli.letsae@vodacom.co.ls>
2024-04-23 16:35:53 +0200

    Final v1

--- ML Pipeline.py
+++ ML Pipeline.py
@@ -14,6 +14,7 @@ import nltk
 import pickle
 nltk.download('punkt') 
 import sqlite3
+import numpy as np
 import os
 import datetime
 import time
@@ -50,7 +51,7 @@ pipeline = Pipeline([
     ('clf', MultiOutputClassifier(RandomForestClassifier()))  # Multi-output classifier
 ])
 print('-----------------------------------------------------------------------------')
-print('Fitting ML pipeline...')
+print('Fitting ML pipeline...(RandomForestClassifier,TfidfVectorizer)')
 ### 4. Train pipeline
 pipeline.fit(X_train, y_train)
 print('-----------------------------------------------------------------------------')
@@ -58,16 +59,16 @@ print('Computing model score...')
 y_pred = pipeline.predict(X_test)
 # Calculate accuracy for each label
 accuracies = [accuracy_score(y_test[label], y_pred[:, idx]) for idx, label in enumerate(y_test.columns)]
-print("Accuracy for each label:", accuracies)
+#print("Accuracy for each label:", accuracies)
 # Calculate Hamming Loss for each label
 hamming_losses = [hamming_loss(y_test[label], y_pred[:, idx]) for idx, label in enumerate(y_test.columns)]
-print("Hamming Loss for each label:", hamming_losses)
+#print("Hamming Loss for each label:", hamming_losses)
 # Calculate Jaccard Score for each label
 jaccard_scores = [jaccard_score(y_test[label], y_pred[:, idx], average=None) for idx, label in enumerate(y_test.columns)]
-print("Jaccard Score for each label:", jaccard_scores)
+#print("Jaccard Score for each label:", jaccard_scores)
 # Calculate F1 Score for each label
 f1_scores = [f1_score(y_test[label], y_pred[:, idx], average=None) for idx, label in enumerate(y_test.columns)]
-print("F1 Score for each label:", f1_scores)
+#print("F1 Score for each label:", f1_scores)
 print('-----------------------------------------------------------------------------')
 print("Classification Report:")
 # Assuming y_test and y_pred are your true labels and predicted labels respectively
@@ -79,7 +80,7 @@ report = classification_report(y_test_flat, y_pred_flat)
 print(report)
 print('-----------------------------------------------------------------------------')
 print("Hyper-parameter tuning...")
-param_grid = {
+"""param_grid = {
     'tfidf__max_features': [1000, 2000, 3000],  # Number of features to consider
     'tfidf__ngram_range': [(1, 1), (1, 2)],      # Range of n-grams
     'clf__estimator__n_estimators': [100, 200, 300],  # Number of trees in the forest
@@ -94,7 +95,7 @@ grid_search.fit(X_train, y_train)
 # Evaluate performance on test set
 best_pipeline = grid_search.best_estimator_
 y_pred = best_pipeline.predict(X_test)
-print("Optimal parameters :",best_pipeline)
+print("Optimal parameters :",best_pipeline) """
 print('-----------------------------------------------------------------------------')
 print("Retraining the model with tuned parameters...")
 # Define pipeline
@@ -111,19 +112,19 @@ print('recomputing model score...')
 y_pred = pipeline.predict(X_test)
 # Calculate accuracy for each label
 accuracies = [accuracy_score(y_test[label], y_pred[:, idx]) for idx, label in enumerate(y_test.columns)]
-print("Accuracy for each label:", accuracies)
+#print("Accuracy for each label:", accuracies)
 
 # Calculate Hamming Loss for each label
 hamming_losses = [hamming_loss(y_test[label], y_pred[:, idx]) for idx, label in enumerate(y_test.columns)]
-print("Hamming Loss for each label:", hamming_losses)
+#print("Hamming Loss for each label:", hamming_losses)
 
 # Calculate Jaccard Score for each label
 jaccard_scores = [jaccard_score(y_test[label], y_pred[:, idx], average=None) for idx, label in enumerate(y_test.columns)]
-print("Jaccard Score for each label:", jaccard_scores)
+#print("Jaccard Score for each label:", jaccard_scores)
 
 # Calculate F1 Score for each label
 f1_scores = [f1_score(y_test[label], y_pred[:, idx], average=None) for idx, label in enumerate(y_test.columns)]
-print("F1 Score for each label:", f1_scores)
+#print("F1 Score for each label:", f1_scores)
 print('-----------------------------------------------------------------------------')
 print("Classification Report(For model with optimal parameters):")
 # Assuming y_test and y_pred are your true labels and predicted labels respectively
@@ -135,30 +136,36 @@ report = classification_report(y_test_flat, y_pred_flat)
 print("Classification Report:")
 print(report)
 print('-----------------------------------------------------------------------------')
-print("Trying a different model with a new text processor:")
+print("Trying a different model with a different text processor - (XGBClassifier,CountVectorizer):")
 # Define pipeline with CountVectorizer and use XGboost classifier
 pipeline_v2 = Pipeline([
     ('count_vectorizer', CountVectorizer()),  # Text preprocessing
     ('clf', MultiOutputClassifier(XGBClassifier()))  # Multi-output classifier
 ])
+# Find the minimum value in y_train
+min_label = y_train.min()
+# Adjust the target labels
+y_train_adjusted = y_train - min_label
+# Fit the pipeline with the adjusted labels
+pipeline_v2.fit(X_train, y_train_adjusted)
 print('-----------------------------------------------------------------------------')
 print("Classification Report(For model with optimal parameters) and scores:")
 y_pred = pipeline_v2.predict(X_test)
 # Calculate accuracy for each label
 accuracies = [accuracy_score(y_test[label], y_pred[:, idx]) for idx, label in enumerate(y_test.columns)]
-print("Accuracy for each label:", accuracies)
+#print("Accuracy for each label:", accuracies)
 
 # Calculate Hamming Loss for each label
 hamming_losses = [hamming_loss(y_test[label], y_pred[:, idx]) for idx, label in enumerate(y_test.columns)]
-print("Hamming Loss for each label:", hamming_losses)
+#print("Hamming Loss for each label:", hamming_losses)
 
 # Calculate Jaccard Score for each label
 jaccard_scores = [jaccard_score(y_test[label], y_pred[:, idx], average=None) for idx, label in enumerate(y_test.columns)]
-print("Jaccard Score for each label:", jaccard_scores)
+#print("Jaccard Score for each label:", jaccard_scores)
 
 # Calculate F1 Score for each label
 f1_scores = [f1_score(y_test[label], y_pred[:, idx], average=None) for idx, label in enumerate(y_test.columns)]
-print("F1 Score for each label:", f1_scores)
+#print("F1 Score for each label:", f1_scores)
 # Assuming y_test and y_pred are your true labels and predicted labels respectively
 
 # Flatten y_test and y_pred to fit classification_report
@@ -172,7 +179,7 @@ print("Classification Report:")
 print(report)
 print('-----------------------------------------------------------------------------')
 print("Parameter tuning for XGboost:")
-pipeline = Pipeline([
+"""pipeline = Pipeline([
     ('count_vectorizer', CountVectorizer()),  # Text preprocessing
     ('clf', MultiOutputClassifier(XGBClassifier()))  # Multi-output classifier
 ])
@@ -182,7 +189,13 @@ min_label = y_train.min()
 
 # Adjust the target labels
 y_train_adjusted = y_train - min_label
-
+param_grid = {
+    'clf__estimator__n_estimators': [50, 100, 200],
+    'clf__estimator__max_depth': [3, 5, 7],
+    'count_vectorizer__max_features': [1000, 5000, 10000],  # Max features for CountVectorizer
+    'count_vectorizer__ngram_range': [(1, 1), (1, 2), (2, 2)],  # N-gram range for CountVectorizer
+    # Add more hyperparameters as needed
+}
 # Initialize GridSearchCV with adjusted target labels
 grid_search = GridSearchCV(estimator=pipeline, param_grid=param_grid, cv=3, scoring='accuracy')
 
@@ -191,12 +204,61 @@ grid_search.fit(X_train, y_train_adjusted)
 
 # Print best parameters and best score
 print("Best Parameters: ", grid_search.best_params_)
-print("Best Score: ", grid_search.best_score_)
-
-# Assuming 'pipeline' is your trained pipeline
-pipeline.fit(X_train, y_train)  # Train your pipeline if not already done
+print("Best Score: ", grid_search.best_score_)"""
 print('-----------------------------------------------------------------------------')
-print("Exporing model artifacts:")
+print("Trying a optimized model - (XGBClassifier,CountVectorizer):")
+# Define pipeline with CountVectorizer and use XGboost classifier
+pipeline_v2 = Pipeline([
+    ('count_vectorizer', CountVectorizer()),  # Text preprocessing
+    ('clf', MultiOutputClassifier(XGBClassifier(clf__estimator__max_depth= 3, clf__estimator__n_estimators= 50, count_vectorizer__max_features= 1000, count_vectorizer__ngram_range= (1, 1))))  # Multi-output classifier
+])
+# Find the minimum value in y_train
+min_label = y_train.min()
+# Adjust the target labels
+y_train_adjusted = y_train - min_label
+# Fit the pipeline with the adjusted labels
+pipeline_v2.fit(X_train, y_train_adjusted)
+print('-----------------------------------------------------------------------------')
+print("Classification Report(For model with optimal parameters) and scores:")
+y_pred = pipeline_v2.predict(X_test)
+# Calculate accuracy for each label
+accuracies = [accuracy_score(y_test[label], y_pred[:, idx]) for idx, label in enumerate(y_test.columns)]
+#print("Accuracy for each label:", accuracies)
+
+# Calculate Hamming Loss for each label
+hamming_losses = [hamming_loss(y_test[label], y_pred[:, idx]) for idx, label in enumerate(y_test.columns)]
+#print("Hamming Loss for each label:", hamming_losses)
+
+# Calculate Jaccard Score for each label
+jaccard_scores = [jaccard_score(y_test[label], y_pred[:, idx], average=None) for idx, label in enumerate(y_test.columns)]
+#print("Jaccard Score for each label:", jaccard_scores)
+
+# Calculate F1 Score for each label
+f1_scores = [f1_score(y_test[label], y_pred[:, idx], average=None) for idx, label in enumerate(y_test.columns)]
+#print("F1 Score for each label:", f1_scores)
+# Assuming y_test and y_pred are your true labels and predicted labels respectively
+
+# Flatten y_test and y_pred to fit classification_report
+y_test_flat = y_test.values.ravel()
+y_pred_flat = y_pred.ravel()
+
+# Generate the classification report
+report = classification_report(y_test_flat, y_pred_flat)
+
+print("Classification Report:")
+print(report)
+print('-----------------------------------------------------------------------------')
+print("Exporting model artifacts:")
 # Serialize the pipeline using pickle
 with open('model.pkl', 'wb') as file:
-    pickle.dump(pipeline, file)
+    pickle.dump(pipeline_v2, file)
+
+
+print("Load completed!")
+print('------------------------------------------------')
+end_time = time.time()
+print("end time",datetime.datetime.now())
+run_time = end_time - start_time
+print("pipe-line ran for",np.round(run_time/60,2),"minutes")
+print('------------------------------------------------')
+    
--- interface.py
+++ interface.py
@@ -1,9 +1,36 @@
-from flask import Flask, render_template
+import pickle
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
+
 app = Flask(__name__)
 
-@app.route("/")
+# Load the model
+with open('model.pkl', 'rb') as file:
+    loaded_model = pickle.load(file)
+
+# Define a function for preprocessing input data
+def preprocess_input(data):
+    # Perform any necessary preprocessing here
+    # Example: Tokenization, vectorization, scaling, etc.
+    return data
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
-if __name__ ==  "__main__" :
+    if request.method == 'POST':
+        # Get input data from the form
+        input_data = [request.form['input_data']]
+        print(input_data)
+        # Preprocess the input data
+        #preprocessed_data = preprocess_input(input_data)
+        # Make prediction
+        prediction = list(loaded_model.predict(input_data))  # Assuming your model expects a list of inputs
+        prediction = prediction[0].tolist()
+        print(prediction)
+        return render_template("index.html", data=prediction)
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
+
 
